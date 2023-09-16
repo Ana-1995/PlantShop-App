8539cc9717370e5c7ee27edd7b19b36701b8b91d
Anano <ananoiashagashvili@gmail.com>
2023-09-16 23:26:21 +0400

    Updated React.js components

--- src/global/banners/CookieBanner.jsx
+++ src/global/banners/CookieBanner.jsx
@@ -1,17 +1,27 @@
-import React from 'react'
-import { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const CookieBanner = () => {
- const [showBanner, setShowBanner] = useState(true)
+  const [showBanner, setShowBanner] = useState(false) 
 
- const hideBanner = () => {
-   setShowBanner(false)
-   localStorage.setItem('cookieBannerShown', 'true') 
- }
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowBanner(true)
+    }, 7000) 
 
- if (!showBanner) {
-   return null
- }
+    return () => {
+      clearTimeout(timer)
+    }
+  }, []) 
+
+  const hideBanner = () => {
+    setShowBanner(false)
+    localStorage.setItem('cookieBannerShown', 'true')
+  }
+
+  if (!showBanner) {
+    return null
+  }
+
   return (
     <div className='flex flex-row justify-between fixed left-0 lg:left-40 bottom-0 lg:bottom-5 w-full lg:w-[80%] rounded-none lg:rounded-md bg-green-800 text-white p-2 lg:p-4 text-center'>
       <p className='text-sm sm:text-lg md:text-md lg:text-sm text-start lg:text-center'>
